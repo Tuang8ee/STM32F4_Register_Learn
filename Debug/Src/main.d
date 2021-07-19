124c7f8d9318afddd6dbcab1d329e3d225a9e376
tuang8ee <tuang8ee@gmail.com>
2021-07-19 17:39:27 +0700

    Update

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,5 +1,6 @@
 Src/main.o: ../Src/main.c \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/main.h \
+ F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/GPIO.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/stm32f407def.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Include/core_cm4.h \
@@ -8,13 +9,14 @@ Src/main.o: ../Src/main.c \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Include/cmsis_gcc.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Include/mpu_armv7.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/GPIO.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/timer.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/UART.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/ADC.h \
  F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/SPI.h
 
 F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/main.h:
+
+F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/GPIO.h:
 
 F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/stm32f407def.h:
 
@@ -31,8 +33,6 @@ F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CM
 F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Include/mpu_armv7.h:
 
 F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
-
-F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/GPIO.h:
 
 F:/Projects/Projects/Work/Work/Learn/STM32_Res/STM32F4_Register_Learn/MyLib/Inc/timer.h:
 
